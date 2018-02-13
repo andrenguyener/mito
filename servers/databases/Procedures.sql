596a2a6f68b5aa4d20f22815967d2a131572d85e
TheRealGuopher <jjguo@uw.edu>
2018-02-12 21:59:22 -0800

    Code for inserting a row into the  NOTIFICATION table when a user sends a friend request to another user

--- servers/databases/Procedures.sql
+++ servers/databases/Procedures.sql
@@ -4,11 +4,12 @@ SELECT * FROM FRIEND
 SELECT * FROM [NOTIFICATION]
 SELECT * FROM NOTIFICATION_TYPE
 
-INSERT INTO NOTIFICATION_TYPE (NotificationType, LinkToView, [Message])
-		VALUES ('Friend Request', 'www.sample.com', 'has sent a friend request')
-
---CREATE PROC insertNotification
---@
+/*
+	Insert data into FRIEND table to record friend connection
+	Insert notification into NOTIFICATION table to record notification sent to @Username2
+	@Username1 sent a friend request to @Username2
+	@FriendType will always be "Pending"
+*/
 ALTER PROC insertFriend
 	@Username1 nvarchar(50),
 	@Username2 nvarchar(50),
@@ -17,30 +18,40 @@ ALTER PROC insertFriend
 	DECLARE @User1Id INT = (SELECT UserId FROM [USER] WHERE Username = @Username1)
 	DECLARE @User2Id INT = (SELECT UserId FROM [USER] WHERE Username = @Username2)
 	DECLARE @FriendTypeId INT = (SELECT FriendTypeId FROM FRIEND_TYPE WHERE FriendType = @FriendType)
+	
+	-- Check both users already exist in the [USER] database
 	IF @User1Id IS NULL OR @User2Id IS NULL
 		BEGIN
 			PRINT 'User1 or User2 does not exist'
 			RAISERROR('@Username1 or @Username2 is null', 11, 1)
 			RETURN
 		END
+
+	-- Check @FriendType exists in FRIEND database
 	IF @FriendTypeId IS NULL
 		BEGIN
 			PRINT 'FriendType does not exist'
 			RAISERROR('@FriendType does not exist', 11, 1)
 			RETURN
 		END
+
+	-- Check @Username1 and @Username2 aren't already friends in FRIEND table
 	IF EXISTS (SELECT * FROM FRIEND WHERE User1Id = @User1Id AND User2Id = @User2Id)
 		BEGIN
 			PRINT 'User1 and User2 are already friends'
 			RAISERROR('@User1Id and @User2Id are already in the table', 11, 1)
 			RETURN
 		END
+
+	-- Reverse check order @Username1 and @Username2 aren't already friends in FRIEND table
 	IF EXISTS (SELECT * FROM FRIEND WHERE User1Id = @User2Id AND User2Id = @User1Id)
 		BEGIN
 			PRINT 'User1 and User2 are already friends'
 			RAISERROR('@User1Id and @User2Id are already in the table', 11, 1)
 			RETURN
 		END
+
+	-- INSERT friendship between @Username1 and @Username2 in FRIEND table
 	BEGIN TRAN
 		INSERT INTO FRIEND (User1Id, User2Id, FriendTypeId) VALUES (@User1Id, @User2Id, @FriendTypeId)
 		DECLARE @FriendId INT = (SELECT SCOPE_IDENTITY())
@@ -48,6 +59,7 @@ ALTER PROC insertFriend
 			ROLLBACK TRAN
 		ELSE
 			COMMIT TRAN
+			-- If @FriendType is a friend request that has not been accepted
 			IF @FriendType = 'Pending'
 				DECLARE @NotificationTypeId INT = (SELECT NotificationTypeId FROM NOTIFICATION_TYPE WHERE NotificationType = @FriendType)
 				IF @NotificationTypeId IS NULL
@@ -56,11 +68,14 @@ ALTER PROC insertFriend
 						RAISERROR('@NotificationTypeId is null',11,1)
 						RETURN
 					END
+				-- Insert notification that @Username1 sent to @Username2
 				INSERT INTO NOTIFICATION (FriendId, NotificationTypeId, SendFrom, NotificationDate)
 						VALUES(@FriendId, @NotificationTypeId, 0, GETDATE())
 
+-- EXEC insert new friendship into [FRIEND] table
 EXEC insertFriend @Username1 = 'andre', @Username2 = 'guopher8', @FriendType = 'Pending'
 
+-- EXEC insert new user into [USER] table
 DECLARE @pwd varbinary(max) = CAST('wat' AS VARBINARY(MAX))
 EXEC insertUser @UserFname = 'Andre', 
 				@UserLname = 'Nguyen', 
@@ -69,7 +84,9 @@ EXEC insertUser @UserFname = 'Andre',
 				@PhotoUrl = NULL,
 				@UserDOB = '03-08-1997',
 				@Username = 'andre'
-
+/*
+	Insert a new user into the [USER] database
+*/
 ALTER PROC insertUser
 @UserFname nvarchar(50),
 @UserLname nvarchar(50),
@@ -79,20 +96,21 @@ ALTER PROC insertUser
 @UserDOB date,
 @Username nvarchar(50)
 AS
+	-- Check username does not already exist
 	IF EXISTS (SELECT * FROM [USER] WHERE Username = @Username)
 			BEGIN
 				PRINT 'Username already exists'
 				RAISERROR('@Username is not unique', 11, 1)
 				RETURN
 			END
-
+	-- Check email does not already exist in the database
 	IF EXISTS (SELECT * FROM [USER] WHERE UserEmail = @UserEmail)
 	BEGIN
 		PRINT 'An account with this email already exists'
 		RAISERROR('@UserEmail is already in the database', 11, 1)
 		RETURN
 	END
-
+	-- Check that new user is at least 13 years old
 	IF DATEDIFF(year,@UserDOB,GetDate()) < 13 
 		BEGIN
 			PRINT 'You must be at least 13 years old.'
