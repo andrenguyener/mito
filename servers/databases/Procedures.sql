7b8ba73cd166b54a716626d23f1721460caa1f20
TheRealGuopher <jjguo@uw.edu>
2018-02-12 21:22:33 -0800

    Delete old insertUsername procedure. Username table no longer exists

--- servers/databases/Procedures.sql
+++ servers/databases/Procedures.sql
@@ -1,59 +1,47 @@
+SELECT * FROM [USER]
+SELECT * FROM FRIEND
+SELECT * FROM FRIEND_TYPE
+
 ALTER PROC insertFriend
-@Username1 nvarchar(50),
-@Username2 nvarchar(50),
-@FriendType nvarchar(25)
-AS
-DECLARE @User1Id INT = (SELECT UserId FROM [USER] WHERE Username = @Username1)
-DECLARE @User2Id INT = (SELECT UserId FROM [USER] WHERE Username = @Username2)
-DECLARE @FriendTypeId INT = (SELECT FriendTypeId FROM FRIEND_TYPE WHERE FriendType = @FriendType)
-IF @User1Id IS NULL OR @User2Id IS NULL
-	BEGIN
-		PRINT 'User1 or User2 does not exist'
-		RAISERROR('@Username1 or @Username2 is null', 11, 1)
-		RETURN
-	END
-IF @FriendTypeId IS NULL
-	BEGIN
-		PRINT 'FriendType does not exist'
-		RAISERROR('@FriendType does not exist', 11, 1)
-		RETURN
-	END
-IF EXISTS (SELECT * FROM FRIEND WHERE User1Id = @User1Id AND User2Id = @User2Id)
-	BEGIN
-		PRINT 'User1 and User2 are already friends'
-		RAISERROR('@User1Id and @User2Id are already in the table', 11, 1)
-		RETURN
-	END
-IF EXISTS (SELECT * FROM FRIEND WHERE User1Id = @User2Id AND User2Id = @User1Id)
-	BEGIN
-		PRINT 'User1 and User2 are already friends'
-		RAISERROR('@User1Id and @User2Id are already in the table', 11, 1)
-		RETURN
-	END
-BEGIN TRAN
-	INSERT INTO FRIEND (User1Id, User2Id, FriendTypeId) VALUES (@User1Id, @User2Id, @FriendTypeId)
-	IF @@ERROR <> 0
-		ROLLBACK TRAN
-	ELSE
-		COMMIT TRAN
-
---EXEC insertFriend @Username1 = 'sneak', @Username2 = 'guopher8', @FriendType = 'Acquaintance'
-
-CREATE PROC insertUserName
-@Username nvarchar(50)
-AS
-	IF EXISTS (SELECT * FROM USERNAME WHERE Username = @Username)
+	@Username1 nvarchar(50),
+	@Username2 nvarchar(50),
+	@FriendType nvarchar(25)
+	AS
+	DECLARE @User1Id INT = (SELECT UserId FROM [USER] WHERE Username = @Username1)
+	DECLARE @User2Id INT = (SELECT UserId FROM [USER] WHERE Username = @Username2)
+	DECLARE @FriendTypeId INT = (SELECT FriendTypeId FROM FRIEND_TYPE WHERE FriendType = @FriendType)
+	IF @User1Id IS NULL OR @User2Id IS NULL
 		BEGIN
-			PRINT 'Username already exists'
-			RAISERROR('@Username is not unique', 11, 1)
+			PRINT 'User1 or User2 does not exist'
+			RAISERROR('@Username1 or @Username2 is null', 11, 1)
+			RETURN
+		END
+	IF @FriendTypeId IS NULL
+		BEGIN
+			PRINT 'FriendType does not exist'
+			RAISERROR('@FriendType does not exist', 11, 1)
+			RETURN
+		END
+	IF EXISTS (SELECT * FROM FRIEND WHERE User1Id = @User1Id AND User2Id = @User2Id)
+		BEGIN
+			PRINT 'User1 and User2 are already friends'
+			RAISERROR('@User1Id and @User2Id are already in the table', 11, 1)
+			RETURN
+		END
+	IF EXISTS (SELECT * FROM FRIEND WHERE User1Id = @User2Id AND User2Id = @User1Id)
+		BEGIN
+			PRINT 'User1 and User2 are already friends'
+			RAISERROR('@User1Id and @User2Id are already in the table', 11, 1)
 			RETURN
 		END
 	BEGIN TRAN
-		INSERT INTO USERNAME(Username) VALUES(@Username)
+		INSERT INTO FRIEND (User1Id, User2Id, FriendTypeId) VALUES (@User1Id, @User2Id, @FriendTypeId)
 		IF @@ERROR <> 0
 			ROLLBACK TRAN
 		ELSE
 			COMMIT TRAN
+
+--EXEC insertFriend @Username1 = 'sneak', @Username2 = 'guopher8', @FriendType = 'Acquaintance'
 
 --DECLARE @pwd varbinary(max) = CAST('wahid2' AS VARBINARY(MAX))
 --EXEC insertUser @UserFname = 'Sopheak', 
