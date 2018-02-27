574652f606ad3559f4bfea057f85149456138d67
Benny Souriyadeth <bennys@uw.edu>
2018-02-27 00:02:39 -0800

    add checkout page, connect cart view to carttableviewcontroller

---
+++ iOS/Mito 1.0/CartTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CartTableViewCell.swift
+//  Mito 1.0
+//
+//  Created by Benny on 2/26/18.
+//  Copyright © 2018 Benny Souriyadeth. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var seller: UILabel!
+    @IBOutlet weak var price: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
--- iOS/Mito 1.0/CartViewController.swift
+++ iOS/Mito 1.0/CartViewController.swift
@@ -11,14 +11,34 @@ import UIKit
 class CartViewController: UIViewController {
 
     
-    @IBAction func backButton(_ sender: Any) {
-        performSegue(withIdentifier: "cartToHome", sender: self)
-    }
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    //User's Cart
+//    @IBOutlet weak var numberItemsInCart: UILabel!
+//    @IBOutlet weak var totalCartPrice: UILabel!
+//    @IBOutlet weak var itemImage: UIImageView!
+//    @IBOutlet weak var itemName: UILabel!
+//    @IBOutlet weak var seller: UILabel!
+//    @IBOutlet weak var price: UILabel!
+    
+    @IBOutlet weak var cartTableView: UITableView!
+    
+    @IBAction func finishShopping(_ sender: Any) {
+        performSegue(withIdentifier: "toCheckout", sender: self)
+    }
+    @IBAction func backButton(_ sender: Any) {
+        performSegue(withIdentifier: "cartToHome", sender: self)
+    }
+    
+    // Checkout Page
+    
+
+    @IBAction func checkoutToCart(_ sender: Any) {
+        performSegue(withIdentifier: "checkoutToCart", sender: self)
+    }
+    
 }
