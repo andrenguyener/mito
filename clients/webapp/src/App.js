34fb2a0c434eeca5efb33b614d0286b8e2a122e8
andre nguyen <andre97@uw.edu>
2018-03-02 11:28:46 -0800

    played around

--- clients/webapp/src/App.js
+++ clients/webapp/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { HashRouter as Router, Switch, Redirect, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Home from './components/home';
 import PropTypes from 'prop-types';
-
+import './styles/app.css';
 // redux set-up: must wrap Router with <Provider> to give components access to redux "store" props 
 const App = ({ store }) => (
   <Provider store={store}>
