5a32f7b3faffe302ec1af5e4f99af1c4189d616a
Benny Souriyadeth <bennys@uw.edu>
2018-03-11 16:01:41 -0700

    add new pictures for app data

--- iOS/Mito 1.0/AppData.swift
+++ iOS/Mito 1.0/AppData.swift
@@ -18,11 +18,12 @@ class AppData: NSObject {
     open var cart: [Product] = []
     
     open var feedItem: [FeedItem] = [
-        FeedItem(avatar: "Sopheak.png", descr: "Testing a multiline comment because we need to test it why not", time: "12h", whatHappened: "Andre Nguyen received a gift from Sopheak Neak"),
-        FeedItem(avatar: "Andre2.png", descr: "Gang Gang", time: "6h", whatHappened: "Andre Nguyen received a gift from Sopheak Neak"),
-        FeedItem(avatar: "Sopheak.png", descr: "Gucci Gang", time: "12h", whatHappened: "Andre Nguyen received a gift from Sopheak Neak"),
-        FeedItem(avatar: "Andre2.png", descr: "Gang Gang", time: "6h", whatHappened: "Andre Nguyen received a gift from Sopheak Neak"),
-        FeedItem(avatar: "Sopheak.png", descr: "Gucci Gang", time: "12h", whatHappened: "Andre Nguyen received a gift from Sopheak Neak"),
-        FeedItem(avatar: "Andre2.png", descr: "Gang Gang", time: "6h", whatHappened: "Andre Nguyen received a gift from Sopheak Neak")
+        FeedItem(avatar: "Sopheak.png", descr: "Ayyyy its finally time for us to ERD!!", time: "10m", whatHappened: "Sopheak Neak received a gift from Andre Nguyen"),
+        FeedItem(avatar: "Andre2.png", descr: "Gang Gang", time: "50m", whatHappened: "Andre Nguyen received a gift from Ammara Touch"),
+        FeedItem(avatar: "ammara.png", descr: "Gucci Gang", time: "12h", whatHappened: "Ammara Touch received a gift from Benny Souriyadeth"),
+        FeedItem(avatar: "benny.png", descr: "Gang Gang", time: "6h", whatHappened: "Benny Souriyadeth received a gift from Avina Vongpradith"),
+        FeedItem(avatar: "avina.png", descr: "Gucci Gang", time: "12h", whatHappened: "Avina Vongradith received a gift from Sarah Phillips"),
+        FeedItem(avatar: "sarah.png", descr: "Gang Gang", time: "6h", whatHappened: "Sarah Phillips received a gift from JJ Guo"),
+        FeedItem(avatar: "jj.png", descr: "Gang Gang", time: "6h", whatHappened: "JJ Guo received a gift from Sopheak Neak")
     ]
 }
