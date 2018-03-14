805520512929967728e722820d2afa68e303adae
Benny Souriyadeth <bennys@uw.edu>
2018-03-13 18:30:41 -0700

    Load from Login.storyboard instead of Main.storyboard

--- iOS/Mito 1.0/AppDelegate.swift
+++ iOS/Mito 1.0/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        self.window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "Open")
         return true
     }
 
