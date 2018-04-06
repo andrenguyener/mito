98cb3729ef2db31d4066a4d3f64390c19c871f58
andre nguyen <andre97@uw.edu>
2018-04-06 01:33:48 -0700

    added some cart handlers

--- servers/mitoNode/models/cart/cart-store.js
+++ servers/mitoNode/models/cart/cart-store.js
@@ -8,15 +8,86 @@ class CartStore {
         this.sql = sql;
     }
 
+    request(procedure) {
+        return new Request((`${procedure}`), function (err) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+
     // Get items in cart
     get() {
 
     }
 
     // Add items to cart
-    insert() {
+    insert(userId, amazonASIN, amazonPrice, quantity) {
+        return new Promise((resolve) => {
+            let procedureName = "uspcInsertIntoCart";
+            var request = this.request(procedureName);
+            request.addParameter('UserId', TYPES.Int, userId);
+            request.addParameter('AmazonASIN', TYPES.Int, amazonASIN);
+            request.addParameter('AmazonPrice', TYPES.Numeric, amazonPrice);
+            request.addParameter('Qty', TYPES.Int, quantity);
+
+            // let jsonArray = []
+            // request.on('row', function (columns) {
+            //     var rowObject = {};
+            //     columns.forEach(function (column) {
+            //         if (column.value === null) {
+            //             console.log('NULL');
+            //         } else {
+            //             rowObject[column.metadata.colName] = column.value;
+            //         }
+            //     });
+            //     jsonArray.push(rowObject);
+            // });
+
+            request.on('doneProc', function (rowCount, more) {
+                // console.log(jsonArray);
+                // resolve(jsonArray);
+                resolve("items added to cart")
+            });
+
+            this.sql.callProcedure(request)
+        })
+            .then((jsonArray) => {
+                return jsonArray;
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+
 
     }
+
+    // process cart items for checkout
+    process(userId, userAddressId, recipientId, message, giftOption) {
+        return new Promise((resolve) => {
+            let procedureName = "uspcProcessCheckout";
+            var request = this.request(procedureName);
+            request.addParameter('UserId', TYPES.Int, userId);
+            request.addParameter('UserAddressId', TYPES.Int, userAddressId);
+            request.addParameter('RecipientId', TYPES.Int, recipientId);
+            request.addParameter('Message', TYPES.NVarChar, message);
+            request.addParameter('GiftOption', TYPES.Int, giftOption);
+
+            request.on('doneProc', function (rowCount, more) {
+                resolve("Checkout completed");
+            })
+
+            this.sql.callProcedure(request);
+        })
+            .then(() => {
+
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+
 
     // Deletes items from cart
     delete() {
