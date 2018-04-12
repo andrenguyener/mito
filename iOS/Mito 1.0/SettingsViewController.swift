3327b3c4b24b59d163ad3ae6210ab5f03d0d44bb
TheRealGuopher <jjguo@uw.edu>
2018-04-11 19:18:38 -0700

    Implement sign out handler in SettingsViewController

--- iOS/Mito 1.0/SettingsViewController.swift
+++ iOS/Mito 1.0/SettingsViewController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import Alamofire
 
 class SettingsViewController: UIViewController {
     var appdata = AppData.shared
+    
+    var urlSignOut = URL(string: "https://api.projectmito.io/v1/sessions/mine")
     //Login
     @IBAction func settingsToHome(_ sender: Any) {
         self.performSegue(withIdentifier: "settingsToMe", sender: self)
     }
     @IBAction func signout(_ sender: Any) {
         performSegue(withIdentifier: "signOut", sender: self)
+        self.fnSignOut()
         UserDefaults.standard.removeObject(forKey: "UserInfo")
         appdata.arrAllUsers.removeAll()
         appdata.arrFriends.removeAll()
@@ -24,6 +28,25 @@ class SettingsViewController: UIViewController {
             print("Data is gone")
         } else {
             print("Data is still here")
+        }
+    }
+    
+    func fnSignOut() {
+        let headers: HTTPHeaders = [
+            "Authorization": UserDefaults.standard.object(forKey: "Authorization") as! String
+        ]
+        Alamofire.request(urlSignOut!, method: .delete, encoding: JSONEncoding.default, headers: headers).validate().responseString { response in
+            switch response.result {
+            case .success:
+                if let dictionary = response.result.value {
+                    print(dictionary)
+                    // Any code for storing locally
+                }
+                
+            case .failure(let error):
+                print("Product could not be added to cart")
+                print(error)
+            }
         }
     }
     
