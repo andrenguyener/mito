ca8da51c21b02401f25339e1e7c781d1f0c9164a
sopheak <sopheakneak@live.com>
2018-04-15 16:03:16 -0700

    added sprocs for getting a friendtype based on given 2 users id and add a column for friends count in uspcGetUserById

--- servers/databases/GetUserById.sql
+++ servers/databases/GetUserById.sql
@@ -6,13 +6,17 @@ Find relevant user information based on the given UserId
 ALTER PROC uspcGetUserById 
 @UserId INT
 As	
-	DECLARE @err_msg NVARCHAR(255)
+	--DECLARE @err_msg NVARCHAR(255)
 	-- SET NOCOUNT ON added to prevent extra result sets from  
     -- interfering with SELECT statements.
 	SET NOCOUNT ON
 	IF EXISTS(SELECT UserId FROM [USER] WHERE UserId = @UserId)
 		BEGIN
-		SELECT UserId, UserFname, UserLname,UserEmail,PhotoUrl, UserDOB, Username FROM [USER] WHERE UserId = @UserId
+		DECLARE @FriendCount INT
+		EXEC dbo.uspGetUserFriendCount @UserId, @Count = @FriendCount OUT
+		SELECT UserId, UserFname, UserLname,UserEmail,PhotoUrl, 
+		UserDOB, Username, @FriendCount AS NumFriends 
+		FROM [USER] WHERE UserId = @UserId
 		--FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER
 		END
 	ELSE
@@ -23,3 +27,6 @@ As
 		END
 
 EXEC sp_rename 'uspGetUserById', 'uspcGetUserById'
+
+--example call
+EXEC dbo.uspcGetUserById 7
