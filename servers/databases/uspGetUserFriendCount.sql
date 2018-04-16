fa82a179323c5a74e1a5b1e42e24c8c368d0a54f
sopheak <sopheakneak@live.com>
2018-04-15 20:59:06 -0700

    modified NumFriends to be an attr to the USER table instead

--- servers/databases/uspGetUserFriendCount.sql
+++ servers/databases/uspGetUserFriendCount.sql
@@ -1,4 +1,4 @@
--- returns the count of friend a user has
+-- (no longer using this) returns the count of friend a user has
 CREATE PROC uspGetUserFriendCount
 @UserId INT,
 @Count INT OUT
@@ -11,3 +11,24 @@ AS
 		AND FriendTypeId = @FriendType)
 	END
 GO
+
+-- making above into a function instead
+
+ALTER FUNCTION fnCalculateFriendCount(@UserId INT)
+RETURNS INT
+AS
+	BEGIN
+	DECLARE @FriendTypeId INT = (SELECT FriendTypeId 
+	FROM FRIEND_TYPE WHERE FriendType = 'Friend')
+	
+	DECLARE @Ret INT = (SELECT COUNT(*) FROM FRIEND 
+		WHERE (User1Id = @UserId OR User2Id = @UserId) 
+		AND FriendTypeId = @FriendTypeId)	
+	RETURN @Ret
+	END
+GO
+
+ALTER TABLE [USER]
+ADD NumFriends AS dbo.fnCalculateFriendCount(UserId)
+
+SELECT * FROM [User]
