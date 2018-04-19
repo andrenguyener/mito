5326ac7f4161aac12f3ab680e8cad833a406c6e2
Benny Souriyadeth <bennys@uw.edu>
2018-04-18 20:55:15 -0700

    add select user storyboard, modify checkout screens

---
+++ iOS/Mito 1.0/CheckoutSelectUserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutSelectUserViewController.swift
+//  Mito 1.0
+//
+//  Created by Benny on 4/18/18.
+//  Copyright © 2018 Benny Souriyadeth. All rights reserved.
+//
+
+import UIKit
+
+class CheckoutSelectUserViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
