8e81034817e5454aed76292e615258ccea2f4def
TheRealGuopher <jjguo@uw.edu>
2018-05-18 15:53:07 -0700

    User can write message cleanly

--- iOS/Mito 1.0/WriteMessageViewController.swift
+++ iOS/Mito 1.0/WriteMessageViewController.swift
@@ -22,25 +22,12 @@ class WriteMessageViewController: UIViewController, UITextViewDelegate {
         appdata.fnDisplaySimpleImage(strImageURL: appdata.personRecipient.avatar, img: imgRecipientProfile, boolCircle: true)
         strRecipientName.text = "\(appdata.personRecipient.firstName) \(appdata.personRecipient.lastName)"
         textviewWriteMessage.text = "What's it for?"
-//        textviewWriteMessage.keyboardDismissMode = .interactive
     }
     
-//    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-//        if(text == "\n") {
-//            textView.resignFirstResponder()
-//            return false
-//        }
-//        return true
-//    }
-    
     func textViewDidChange(_ textView: UITextView) {
-        
-        print(textView.text)
         if(textView.text.contains("\n")) {
             textView.resignFirstResponder()
-//            return false
         }
-//        return true
     }
 
     override func didReceiveMemoryWarning() {
