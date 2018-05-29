be827e4845393d8b957685af79b2be241d066fc5
Benny Souriyadeth <bennys@uw.edu>
2018-05-28 19:10:17 -0700

    searching new product now scrolls to top of tableview

--- iOS/Mito 1.0/SearchProductsViewController.swift
+++ iOS/Mito 1.0/SearchProductsViewController.swift
@@ -62,6 +62,12 @@ class SearchProductsViewController: UIViewController, UITableViewDataSource, UIT
         performSegue(withIdentifier: "segSearchProductToCart", sender: self)
     }
     
+//    func scrollToFirstRow() {
+//        let indexPath = NSIndexPath(row: 0, section: 0)
+//        self.productTableView.scrollToRow(at: indexPath as IndexPath, at: .top, animated: true)
+//    }
+    
+
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if (searchBar.text!.replacingOccurrences(of: " ", with: "").count > 0) { // tests for only spaces
             intPageNumber = 1
@@ -71,6 +77,7 @@ class SearchProductsViewController: UIViewController, UITableViewDataSource, UIT
             strProductQuery = ""
             strProductQuery = searchBar.text!
             fnLoadEbayProductData(strCodedSearchQuery: searchBar.text!.replacingOccurrences(of: " ", with: "%20"))
+            
         } else {
             strProductQuery = searchBar.text!.replacingOccurrences(of: " ", with: "")
             searchBar.text! = ""
@@ -80,10 +87,11 @@ class SearchProductsViewController: UIViewController, UITableViewDataSource, UIT
         searchBar.resignFirstResponder()
     }
     
+    
     func fnLoadEbayProductData(strCodedSearchQuery: String) {
         let urlEbayProductCall = URL(string: "https://api.ebay.com/buy/browse/v1/item_summary/search?q=\(strCodedSearchQuery)")
         appdata.arrEbaySearchResults.removeAll()
-        let strBearerToken = "Bearer v^1.1#i^1#p^1#r^0#I^3#f^0#t^H4sIAAAAAAAAAOVXXWwUVRTubtuFisWYgBCsyTJASCkze2dmZ7szdjdZWqDlp13Y2mBR6/zc6V67OzOZO0u7iSalRqKi/NgHiRjFoAbkQRCJxAetYkIkPBDFhEjhwfiAotEoBNSg3pldyrYQfouQuC+bOffcc8/3ne+cmQv6A1Xz1jWvO1ftm+Df1g/6/T4fOwlUBSrrJpf7Z1SWgRIH37b+2f0VA+WnGrCczVjSSogt08Aw2JfNGFjyjDEqZxuSKWOEJUPOQiw5qpRKLF8mcQyQLNt0TNXMUMGWphglhlkAo3xUF+uFcD3UidW4GLPdjFGsEuHUsBCOagpxgCxZxzgHWwzsyIYTozjARmkg0JzYDlgJ8FI4woiA76SCHdDGyDSICwOouJeu5O21S3K9eqoyxtB2SBAq3pJYlGpLtDQtbG1vCJXEihd5SDmyk8OjnxpNDQY75EwOXv0Y7HlLqZyqQoypULxwwuigUuJiMjeRfoHqCFQEJarLYV4Ro5FxYXKRaWdl5+ppuBak0brnKkHDQU7+WoQSMpSnoOoUn1pJiJamoPu3IidnkI6gHaMWLkg8mkgmqXjKtNJQ7mmlk7bp7lpOJ1c20QKnqooGIaTDohzhoBIuHlSIVmR5zEmNpqEhlzMcbDWdBZBkDUdzAyShhBvi1Ga02QndcTMq9eOLHEbFaKdb00IRc07acMsKs4SIoPd47QqM7HYcGyk5B45EGLvgURSjZMtCGjV20ZNiUT19OEalHceSQqHe3l6ml2dMuzvEAcCGVi1fllLTMCtTrq/b654/uvYGGnlQVEh2YiQ5eYvk0kekShIwuqk4Fw2zvFjkfXRa8bHWywwlmEOjG2K8GoTXoKIJQOQ5Tmej9fx4dEi8KNKQmwdU5Dydle0e6FgZWYW0SnSWy0IbaRIv6BwZg5DWIqJOFKvrtCJoEZrVIQQQKooqRv9PjXK9Uk+ppgWTZgap+fER/HiJnbe1pGw7+RTMZIjhelV/RZDYBXn74bm9fiMQ3RiYBJEtxLjaZlQzGzJlMtRcU5eX9S3hRuR1eFcVlQAsIEVa4UXGeHAZvEZlbIjNnE1e4UybO9fbzR5okC5xbDOTgXYHe0tMjONEvzPT/Iqo1AwiNHbdbchucEzepLZl506irhjwdV6OnBW4ekEQolzklrA1enVtz/8XQ+tGCttsYgdqt+EDJDT6NhQv837sgG8fGPDtIRcqEAJz2FlgZqD8kYrye2dg5EAGyTqDUbdBPvJtyPTAvCUj2x/wra7ZvbOr5P617XEwfeQGVlXOTiq5joGaSyuV7H3TqtkoEDgRsIAPRzrBrEurFewDFVO+e7ViymtLa7ccPLb72c/ef6xq8QZ0AlSPOPl8lWVEGGVHFp4KTB7O1Qzu/2vThT+PVv+4bONQ44Vhe6llTTs5AX35/JuZ+MT1q/atb31pA7M3uX9j+a63areuFjf1nql7uebB3McTH/r8TMfU5uO/5n/wHTs/hOZ2n07+je85Mf/QkVjb17XzPpid/uidhmY0NOl3/zfTugLDbd/+3FPdZW08nEYBoaFd+SO7Vh9a8t79R6cvMfd+RR8+8MI/m89WS+vXWnWvH39x/tqpxoq3f7sAl4Z34yfWbK7t5E++4quZ4F98fo+p7Jh3+qeelZXSUNw6cHZw+2B65/FAzbk9W57bcmj7J9/PX+x8+OnTO/oCb3yx/8k5/oOVfQ11tb/EHp777qz8puGts5+Zuau1UL5/ATyMVm0ZDwAA"
+        let strBearerToken = "Bearer v^1.1#i^1#f^0#r^0#p^1#I^3#t^H4sIAAAAAAAAAOVXa2wUVRTubh9SS7USA6ZCWKaCj2Zm78zuDDsTdmGhVDah7dottVZIncfdduzszHbuXdqNEZtqeZgGNcbEGKNNjG+oVovhoTFRovgI0hiD0cQgMUpQfEQTTAzgnelStoXwLELi/tnMueeee77vfOc+QG9J6R3rV6w/Wu65xjvYC3q9Hg9bBkpLiquvK/RWFheAPAfPYO8tvUV9hYcWITllpKVGiNKWiaCvJ2WYSHKNYSpjm5IlIx1JppyCSMKqlIjWrZQ4Bkhp28KWahmUL1YTprRgQGBFQWVZwItcUiRW82TMJitMyUEAeIUVFCEQ4FlRJeMIZWDMRFg2cZjiABuiAU9zYhPgJMBKLMuEOKGV8jVDG+mWSVwYQEXcdCV3rp2X69lTlRGCNiZBqEgsWptoiMZqltc3LfLnxYrkeEhgGWfQxK9llgZ9zbKRgWdfBrneUiKjqhAhyh8ZW2FiUCl6MpmLSN+lOhCSoRZYKATEEB/ixClhstayUzI+exqORdfopOsqQRPrOHsuQgkZyv1QxbmvehIiVuNz/u7KyIae1KEdppYvjd4TjcepSMJKd0C5s56O25Yzq46ON9bQPKeqigYhpIOiLHBQCeYWGouWY3nSSsssU9MdzpCv3sJLIckaTuSGlfg8bohTg9lgR5PYySjfLzjOIWh1ajpWxAzuMJ2ywhQhwud+nrsC47MxtnUlg+F4hMkDLkWka9JpXaMmD7pSzKmnB4WpDozTkt/f3d3NdAcYy273cwCw/pa6lQm1A6ZkyvF1et311889gdZdKCokM5Eu4Wya5NJDpEoSMNupCBcKsgExx/vEtCKTracZ8jD7JzbEVDWICjQBKIIAg0FV4Fk4FR0SyYnU7+QBFTlLp2S7E+K0IauQVonOMilo65oU4JNcIJSEtCaISaLYZJJWeE2g2SSEAEJFUcXQ/6lRzlfqCdVKw7hl6Gp2agQ/VWIP2FpctnE2AQ2DGM5X9WcEiRyQlx+e0+sXAtGJgUgQOa0zjrYZ1Ur5LZlsao6pzc36knDr5Di8qopKAI4h1bWxg4xx4TJorcrYEFkZmxzhTIOzrzdZndAkXYJtyzCg3cxeEhNTuKNfmd38jKhUQyc0tl1tyC5wm7xIbcv4SqIu6vO0no6c5bmFPLmIL7w0tS5z69qU/S82rQsp7AoLYahdhguIf+JrKFLg/tg+zzbQ5xkmDyrgB/PZKjCvpHBVUeH0SqRjyOhykkF6u0ku+TZkOmE2Leu2t8Rz7+w3X23Le38NrgE3jb/ASgvZsrznGJh9aqSYvX5WORsCPCcCcsCybCuoOjVaxM4suvHA8eqIZzhy4rb75le0PtR4+527E6+D8nEnj6e4gAijoKzj4eq3vVvann+jSF2z1h7KvDNv/1PPfh3cqRe/UnZQqIoll+75pmXXJzM+rJ2zpWxk8QO/3rBjH/PbLzePhku+ekuhn3xttZFIdLR837Wr/+nhW19sTqwaGaiuWFQaPDKq/ryj6znKeOH4yrlCtqr/yO4vu09Q2w+q5oIlx+Yc8srm1vLHd69dvW60Ij6nAjwRC287Pmt0Q+O+zZ0fLHn5M/jogc6emfUl/dPuNlJ/TB/8fMPejwZG5m7uX127t1TcOnL0sXWVlUp3y7VmxcfhPT9s2vdghfndez+1b/xrSHimRvl72qft24sHFvye3Vn3T9fiw96hL94vf+nPeTM2Mu/WdH27af/hH4/RLY+Mle9fsFEU0hkPAAA="
         let headers: HTTPHeaders = [
             "Authorization": strBearerToken
         ]
@@ -93,8 +101,8 @@ class SearchProductsViewController: UIViewController, UITableViewDataSource, UIT
                 if let dictionary = response.result.value {
                     let dict = dictionary as! NSDictionary
                     let prettyDict = dictionary as! Dictionary<String, AnyObject>
-                    print(prettyDict.prettyPrint())
-                    print(dict)
+                   // print(prettyDict.prettyPrint())
+                    //print(dict)
                     if dict["total"] as! Int != 0 {
                         let arrItemSummaries = dict["itemSummaries"] as! NSArray
                         var index = 0
@@ -125,13 +133,14 @@ class SearchProductsViewController: UIViewController, UITableViewDataSource, UIT
                             self.appdata.arrEbaySearchResults.append(objEbay)
                             index += 1
                         }
-                        print(self.appdata.arrEbaySearchResults[0].values())
+                        //print(self.appdata.arrEbaySearchResults[0].values())
                     }
                 }
                 DispatchQueue.main.async {
                     self.productTableView.reloadData()
                     self.spinnerProductSearch.stopAnimating()
                     self.spinnerProductSearch.isHidden = true
+                    self.scrollToFirstRow()
                 }
                 
             case .failure(let error):
@@ -149,6 +158,11 @@ class SearchProductsViewController: UIViewController, UITableViewDataSource, UIT
         }
     }
     
+    func scrollToFirstRow() {
+        let indexPath = NSIndexPath(row: 0, section: 0)
+        self.productTableView.scrollToRow(at: indexPath as IndexPath, at: .top, animated: false)
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("Product count: \(appdata.arrProductSearchResults.count)")
         return appdata.arrEbaySearchResults.count
@@ -187,3 +201,4 @@ class SearchProductsViewController: UIViewController, UITableViewDataSource, UIT
         return arrTemp[0] as! String
     }
 }
+
