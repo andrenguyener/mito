b64ba8e80037b935f5db48a127beede6c39e630b
andre nguyen <andre97@uw.edu>
2018-09-13 12:20:31 -0700

    removed files

--- clients/prototype/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-chmod +x build.sh
-./build.sh
-docker push andrenguyener/mitoprototype
-chmod +x provision.sh
-
-ssh -oStrictHostKeyChecking=no root@projectmito.io 'bash -s' < provision.sh
